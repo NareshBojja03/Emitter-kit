cb9dd1e411a980604d7213d765187a74d6e00d14
NareshBojja03 <naresh.bojja@valuelabs.com>
2024-06-11 11:59:02 +0530

    updated Emitter Kit folder

--- Package.swift
+++ Package.swift
@@ -1,31 +1,22 @@
-// swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.3
 import PackageDescription
-
-struct PackageMetadata {
-    static let version: String = "v1.0.1"
-    static let checksum: String = "67815317bf9cd0040e80eddf76360a5b7be29ad8a6b9afcf6f8726c72151a1f1"
-}
 
 let package = Package(
     name: "EmitterKit",
-    platforms: [
-        .iOS(.v9),
-        .macOS(.v10_10),
-        .tvOS(.v9)
-    ],
     products: [
         .library(
             name: "EmitterKit",
             targets: ["EmitterKit"]),
     ],
+    dependencies: [],
     targets: [
-        .binaryTarget(
+        .target(
             name: "EmitterKit",
-            url: "https://github.com/NareshBojja03/Emitter-kit/releases/download/v1.0.1/EmitterKit.xcframework.zip",
-            checksum: "67815317bf9cd0040e80eddf76360a5b7be29ad8a6b9afcf6f8726c72151a1f1"
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+            dependencies: [],
+            path: "Sources/EmitterKit"),
+        .testTarget(
+            name: "EmitterKitTests",
+            dependencies: ["EmitterKit"],
+            path: "Tests/EmitterKitTests"),
+    ]
 )
